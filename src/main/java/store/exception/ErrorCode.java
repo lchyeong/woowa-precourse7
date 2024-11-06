f4536b603146c90251c62f0e9601d999b555e296
lchyeong <lchyeong@gmail.com>
2024-11-06 21:57:54 +0900

    feat(ErrorCode): 에러코드와 메시지 전역처리를 위한 클래스 추가

---
+++ src/main/java/store/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package store.exception;
+
+public enum ErrorCode {
+    CODE("E001", "[ERRE] ㅎㅇ");
+
+    private final String code;
+    private final String message;
+
+    ErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
