01bce98422ccdb5083a5e1601e3816341d876732
lchyeong <lchyeong@gmail.com>
2024-11-11 08:59:46 +0900

    feat(Application): 앱 실행을 위한 로직 추가

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,12 @@
 package store;
 
+import store.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+
+        appConfig.getDataInitializer().init();
+        appConfig.getStoreController().start();
     }
 }
